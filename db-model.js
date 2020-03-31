31f26a9f9e771700ddf45fba1fe20d61fad254e9
Max Stuart <maxastuart@gmail.com>
2020-03-31 01:50:38 -0700

    update db model to ease converting to CSV for NKH

--- db-model.js
+++ db-model.js
@@ -2,12 +2,7 @@ const dbModel = [
   {
     siteName: 'A commonly recognized name for the food distribution site.',
     siteStatus: 'Open', // Status of the food distribution site, one of​ Open​ or C​losed.
-    siteAddress: {
-      streetAddress: '1234 example street',
-      city: '',
-      state: '',
-      zip: 90210
-    },
+    siteAddress: '1234 example street, San Diego, CA 90210',
     siteState: 'CA',
     siteZip: 90210, // i don't know why we need this if we have lat/lng - Max
     contactPhone: 8001234567, // Phone number, formatted as a 10-digit string with no spaces or extra characters.
@@ -20,7 +15,7 @@ const dbModel = [
     snackTimePM: '', // Time range afternoon snacks are being served i.e. 3​ :30 PM- 4:30 PM​.
     dinnerSupperTime: '', // Time range dinner/supper is being served i.e. 5​ :00 PM- 8:00 PM​.
 
-    // Below fields are not for No Kid Hungry CSV export, just for the database & Algolia.
+    // Below fields are not for No Kid Hungry CSV export, just for the database (Algolia).
 
     _geoloc: { // Site latitude and longitude in the WGS84/4326 coordinate system.
       lat: 37.75104109999999,
