4185ea26be9a1e3362d165770af8cc1c4b806531
trevor-ofarrell <744@holbertonschool.com>
2020-04-04 14:29:16 -0700

    adding Google sheet scraper for Indiana

--- node/indiana_scraper/getDateTime.js
+++ node/indiana_scraper/getDateTime.js
@@ -1,36 +1,30 @@
 function getDateTimeNow() {
+  // get current datetime in yyyy/mm/dd HH/mm/ss
 
-    //get current datetime in yyyy/mm/dd HH/mm/ss
+  const date = new Date();
+  const aaaa = date.getFullYear();
+  let gg = date.getDate();
+  let mm = (date.getMonth() + 1);
 
-    var date = new Date();
-    var aaaa = date.getFullYear();
-    var gg = date.getDate();
-    var mm = (date.getMonth() + 1);
+  if (gg < 10) gg = `0${gg}`;
 
-    if (gg < 10)
-        gg = "0" + gg;
+  if (mm < 10) mm = `0${mm}`;
 
-    if (mm < 10)
-        mm = "0" + mm;
+  const cur_day = `${aaaa}-${mm}-${gg}`;
 
-    var cur_day = aaaa + "-" + mm + "-" + gg;
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
 
-    var hours = date.getHours()
-    var minutes = date.getMinutes()
-    var seconds = date.getSeconds();
+  if (hours < 10) hours = `0${hours}`;
 
-    if (hours < 10)
-        hours = "0" + hours;
+  if (minutes < 10) minutes = `0${minutes}`;
 
-    if (minutes < 10)
-        minutes = "0" + minutes;
+  if (seconds < 10) seconds = `0${seconds}`;
 
-    if (seconds < 10)
-        seconds = "0" + seconds;
-
-    return cur_day + " " + hours + ":" + minutes + ":" + seconds;
+  return `${cur_day} ${hours}:${minutes}:${seconds}`;
 }
 
 module.exports = {
-    getDateTimeNow
-}
+  getDateTimeNow,
+};
